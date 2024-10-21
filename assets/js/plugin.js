cec207c03b55947238a2e5132d0c994c98d1ddd7
jubair <you@exampl.com>
2024-10-21 21:48:26 +0600

    ADDED: local

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -8,40 +8,4 @@ AOS.init({
   once: true, // Whether animation should happen only once
 });
 import "preline";
-// import "./custom";
-let date = document.getElementById("date");
-let hour = document.getElementById("hours");
-let mins = document.getElementById("minutes");
-let sec = document.getElementById("seconds");
-
-setInterval(() => {
-  let today = new Date().toLocaleString("en-US", { timeZone: "Asia/Dhaka" });
-  today = new Date(today);
-  // Date
-  let d = today.getDate();
-  let m = today.getMonth() + 1;
-  let y = today.getFullYear();
-  // Time parts
-  let h = today.getHours().toString().padStart(2, "0");
-  let min = today.getMinutes().toString().padStart(2, "0");
-  let s = today.getSeconds().toString().padStart(2, "0");
-  // Update time
-  hour.innerText = h;
-  mins.innerText = min;
-  sec.innerText = s;
-
-  // Update date
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let dayName = daysOfWeek[today.getDay()];
-  date.innerText = `${dayName} ${d} ${today.toLocaleString("default", {
-    month: "long",
-  })} ${y}`;
-}, 1000);
+import "./custom";
